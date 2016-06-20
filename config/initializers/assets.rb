9725d5f09c33dd89368a3a5699fa523251fdc03f
Nikola Milojevic <nmilojevic@gmail.com>
2016-06-20 15:14:35 +0200

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,11 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( dhtmlxscheduler.css )
+Rails.application.config.assets.precompile += %w( business-casual.css )
 Rails.application.config.assets.precompile += %w( dhtmlxscheduler.js )
 Rails.application.config.assets.precompile += %w( ext/dhtmlxscheduler_recurring.js )
 Rails.application.config.assets.precompile += %w( ext/dhtmlxscheduler_quick_info.js )
 Rails.application.config.assets.precompile += %w( ext/dhtmlxscheduler_readonly.js )
-Rails.application.config.assets.precompile.push(Proc.new { |path, fn| fn =~ /app\/assets\/(stylesheets|javascripts).*/})
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
