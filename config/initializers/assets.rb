3b3501e74963268d6d2cd532ed5af60fb2084bdf
Nikola Milojevic <nmilojevic@gmail.com>
2016-06-20 14:46:27 +0200

    no message

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += ['*.js', '*.css', '*.css.scss'] 
+Rails.application.config.assets.precompile += ['*.js', '*.css'] 
 Rails.application.config.assets.precompile += %w( dhtmlxscheduler.css )
 Rails.application.config.assets.precompile += %w( dhtmlxscheduler.js )
 Rails.application.config.assets.precompile += %w( ext/dhtmlxscheduler_recurring.js )
