a5993e02561c283d59af684ec5ba4fe0b9619556
Nikola Milojevic <nmilojevic@gmail.com>
2016-06-20 20:26:49 +0200

    Add more event types

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  enum event_type: [:crossfit, :bootcamp]
+  enum event_type: [:crossfit, :bootcamp, :weightlifting, :power_joga, :open_jim]
 
   has_and_belongs_to_many :users
 
