4f45ea0b1a05977cd478ac93b25b598a4bb95113
Nikola Milojevic <nmilojevic@gmail.com>
2016-06-21 17:01:53 +0200

    Fix - update membership date initially on user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   end 
 
   def send_admin_mail
+    self.update(membership_updated: Time.current)
     CustomerMailer.signup_confirmation_email(self).deliver
   end
 
