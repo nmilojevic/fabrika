46a0cd7c5cb806457ed0991d5b5a7a8b286b2ad7
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-03 19:46:17 +0200

    schedule

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_only, :except => :show
   def show
     render template: "pages/#{params[:page]}"
   end
 
   def schedule
     render template: "pages/schedule"
+  end
+
+  private
+
+  def admin_only
+    unless current_user.admin?
+      redirect_to :back, :alert => "Access denied."
+    end
   end
 end
