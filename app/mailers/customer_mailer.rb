3900765cb58bd6def3b9cd44f7b42b35d3a1c089
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-03 23:10:22 +0200

    New email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,28 +1,34 @@
 class CustomerMailer < ApplicationMailer
 
-  default from: 'fabrika-crossfit@gmail.com'
+  default from: 'crossfit.fabrika@gmail.com'
  
   def account_approved_email(user)
     @user = user
-    @email = ENV['FABRIKA_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
     mail(to: @user.email, subject: 'Your account is approved')
   end
 
   def signup_confirmation_email(user)
     @user = user
-    @email = ENV['FABRIKA_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
     mail(to: @user.email, subject: 'Welcome to Fabrika!')
+  end
+
+  def new_web_site_email
+    @user = user
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
+    mail(to: @user.email, subject: 'Fabrika ima novu aplikaciju!')
   end
 
   def membership_expires_soon_email(user)
     @user = user
-    @email = ENV['FABRIKA_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
     mail(to: @user.email, subject: 'Your membership will expire in 5 days')
   end
 
   def membership_expired(user)
     @user = user
-    @email = ENV['FABRIKA_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
     mail(to: @user.email, subject: 'Your membership has expired')
   end
 
