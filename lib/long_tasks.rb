abd640e46abab4befc870e73577c7be958afc089
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-04 13:44:19 +0200

    Fix long task

--- lib/long_tasks.rb
+++ lib/long_tasks.rb
@@ -1,18 +1,18 @@
 class LongTasks
 
   def expire_members
-    User.where(approved:true, membership_expired: false, role: User.roles[:user]).where("membership_updated_at <= ?", Time.current - 35.days).each do |user|
+    User.active.user.where("membership_updated_at <= ?", Time.current - 35.days).each do |user|
       CustomerMailer.membership_expired_email(user).deliver
       user.expired!
     end 
   end
-  handle_asynchronously :expire_members, :run_at => Proc.new { 12.hours.from_now }
+  handle_asynchronously :expire_members, :run_at => Proc.new { 1.hours.from_now }
  
   def warn_members
-    User.where(approved:true, membership_expired: false, role: User.roles[:user]).where("membership_updated_at <= ?", Time.current - 30.days).each do |user|
+    User.active.user.where("membership_updated_at <= ?", Time.current - 30.days).each do |user|
       CustomerMailer.membership_expires_soon_email(user).deliver
     end
   end
-  handle_asynchronously :warn_members, :run_at => Proc.new { 12.hours.from_now }
+  handle_asynchronously :warn_members, :run_at => Proc.new { 1.hours.from_now }
 
 end
