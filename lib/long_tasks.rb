21cebc0e1e80440d15d64e749b5172abcdd99fd5
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-04 16:02:04 +0200

    fix long task queues.

--- lib/long_tasks.rb
+++ lib/long_tasks.rb
@@ -6,13 +6,13 @@ class LongTasks
       user.expired!
     end 
   end
-  handle_asynchronously :expire_members, :run_at => Proc.new { 1.hours.from_now }
+  handle_asynchronously :expire_members, :queue => 'expire_members', :run_at => Proc.new { 1.minutes.from_now }
  
   def warn_members
     User.active.user.where("membership_updated_at <= ?", Time.current - 30.days).each do |user|
       CustomerMailer.membership_expires_soon_email(user).deliver
     end
   end
-  handle_asynchronously :warn_members, :run_at => Proc.new { 1.hours.from_now }
+  handle_asynchronously :warn_members, :queue => 'warn_members', :run_at => Proc.new { 1.minutes.from_now }
 
 end
