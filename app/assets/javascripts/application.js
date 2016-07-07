4a0737dd7b750138d97bb384009b96ccae7f9ac6
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-07 12:46:18 +0200

    Add submenus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,13 @@ $(function(){
   //override the use of js alert on confirms
   //requires bootstrap3-dialog from https://github.com/nakupanda/bootstrap3-dialog
 
+    $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+      event.preventDefault(); 
+      event.stopPropagation(); 
+      $(this).parent().siblings().removeClass('open');
+      $(this).parent().toggleClass('open');
+    });
+ 
 
   $.rails.allowAction = function(link){
     if( !link.is('[data-confirm]') )
