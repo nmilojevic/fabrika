514dc05dc4a7ded68da0c2131034118984d84102
Nikola Milojevic <nmilojevic@gmail.com>
2016-07-25 15:47:54 +0200

    New custom email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -4,31 +4,31 @@ class CustomerMailer < ApplicationMailer
  
   def account_approved_email(user)
     @user = user
-    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "crossfit.fabrika@gmail.com"
     mail(to: @user.email, subject: 'Vaš nalog je aktiviran.')
   end
 
   def signup_confirmation_email(user)
     @user = user
-    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "crossfit.fabrika@gmail.com"
     mail(to: @user.email, subject: 'Dobrodošli u fabriku!')
   end
 
   def new_web_site_email(user)
     @user = user
-    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
-    mail(to: @user.email, subject: 'Fabrika ima novu aplikaciju!')
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "crossfit.fabrika@gmail.com"
+    mail(to: @user.email, subject: 'Dobrovoljno davanje krvi')
   end
 
   def membership_expires_soon_email(user)
     @user = user
-    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "crossfit.fabrika@gmail.com"
     mail(to: @user.email, subject: 'Vaša članarina je istekla, vaš nalog će biti deaktiviran za 5 dana')
   end
 
   def membership_expired_email(user)
     @user = user
-    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "fabrika.crossfit@gmail.com"
+    @email = ENV['FABRIKA_CONTACT_EMAIL'] || "crossfit.fabrika@gmail.com"
     mail(to: @user.email, subject: 'Vaš nalog je deaktiviran.')
   end
 
