c25dba4633fcfa2afcd28e68ac37340239ab02d5
Nikola Milojevic <nmilojevic@gmail.com>
2016-12-12 12:05:42 +0100

    Fix past date for morning

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   enum event_type: [:crossfit, :bootcamp, :weightlifting, :power_yoga, :open_gym]
 
   has_and_belongs_to_many :users
-  
+
   def reserved_for?(user)
     users.exists?(user)
   end
@@ -15,7 +15,7 @@ class Event < ActiveRecord::Base
     if open_gym?
       @past = end_date < Time.current + 1.hour 
     elsif start_date.hour < 9
-      @past = start_date < Time.current - 8.hour
+      @past = start_date < Time.current + 8.hour
     else
       @past = start_date < Time.current + 1.hour 
     end
